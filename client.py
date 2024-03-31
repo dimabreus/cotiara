30aae5683725b440eecba729324fcb285b5e2ea5
dimabreus <dmitriybreus5@gmail.com>
2024-03-31 14:31:29 +0300

    added api for interpretation

---
+++ client.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+# URL вашего API
+url = 'http://localhost:3000/run'
+
+# Код JavaScript, который вы хотите выполнить
+code = """
+var a = 1 + 1
+
+echo %a%
+"""
+
+# Формирование тела запроса
+payload = json.dumps({
+    'code': code
+})
+
+# Заголовки запроса
+headers = {
+    'Content-Type': 'application/json'
+}
+
+# Отправка POST-запроса
+response = requests.post(url, headers=headers, data=payload)
+
+# Вывод ответа от сервера
+print(response.json())
